a4de5ec34f56101fdc81ee7e5f78c787c6edf734
Sergey Belotelkin <serwhite@yandex.ru>
2013-09-27 10:10:59 +0400

    init project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Photogallery::Application.config.secret_key_base = 'faaa320a5d935892ab69e6f3daa4057d1c810fad3347b42a92aa47764e7cf0b9949c7a1682032943d93b24b63b32cbc2d533f3da9322d06e198f17261ddcef60'
