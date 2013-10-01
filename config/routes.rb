b8438576521f07e0d27e19898fd6a5bb08450d64
Sergey Belotelkin <sergey.belotelkin@gmail.com>
2013-10-01 21:36:06 +0400

    many changes

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Photogallery::Application.routes.draw do
 
   resources :albums, only: [:show] do
     member do
-      get '/:theme', action: :show, constraints: { theme: /folio|azur/ }
+      get '/:theme', action: :show, constraints: { theme: /folio|azur/ }, as: :theme
     end
   end
 
